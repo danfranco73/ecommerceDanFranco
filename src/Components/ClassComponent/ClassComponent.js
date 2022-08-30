237be1742ed5bdb49eb77c60f61f354080ee92f5
danfranco73 <danielfrancotucu@gmail.com>
2022-08-29 22:42:45 -0300

    clase 3

---
+++ src/Components/ClassComponent/ClassComponent.js
@@ -0,0 +1,14 @@
+import{Component}from 'react';
+class ClassComponent extends Component{
+     tender(){
+        return(
+            <div>
+                <h2>
+                    Hola soy un componente de clase
+                </h2>
+            </div>
+        )
+     }
+}
+
+export default ClassComponent;
